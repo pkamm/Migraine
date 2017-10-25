960d7b5c90d7bc80e8c81b3ecf9ad2e373ac42e7
Pete Kamm <kammpet@macc02r214pg8wl.prd.manulifeusa.com>
2017-10-25 13:40:41 -0400

    Initial Commit

---
+++ Migraine/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Migraine
+//
+//  Created by Kamm, Peter on 10/25/17.
+//  Copyright © 2017 MIT. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
