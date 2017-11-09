a594e2e3c9e2822ebed399a48b4725403d7fc414
pkamm <sprizzla@gmail.com>
2017-11-09 16:21:31 -0500

    added login and create account pages

---
+++ Migraine/ViewControllers/LogInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LogInViewController.swift
+//  Migraine
+//
+//  Created by Peter Kamm on 11/9/17.
+//  Copyright © 2017 MIT. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Migraine/ViewControllers/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  Migraine
+//
+//  Created by Peter Kamm on 11/9/17.
+//  Copyright © 2017 MIT. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+
+}
